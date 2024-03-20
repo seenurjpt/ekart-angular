3233c7646b6c9c7af5215b9e8bb9f989b4d2672c
Sunny Rajput <rajputsunny1027@gmail.com>
2024-03-20 12:47:44 +0530

    github code reference in footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: '[app-footer]',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TooltipModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
